a67dbcaeab66543ea40f551cd6095de87ba89725
Alec Magnet <amagnet@gmail.com>
2021-06-28 17:16:40 -0400

    commit tree 039c33393dee65529e044858fd3dc073a8d3a5db

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideLength){
   // Write your code here!
+  if (rideLength < 400) {
+    return ("This one is on me!")
+  } else if (rideLength > 2500) {
+    return ("No can do.")
+  } else if (rideLength > 2000) {
+    return ("I will gladly take your thirty bucks.")
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const cityMessage = city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return cityMessage;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip == 'generous') {
+    return ('Thank you so much.')
+  } else if (tip == 'not as generous') {
+    return ('Thank you.')
+  } else {
+    return ('Bye.')
+  }
 }
